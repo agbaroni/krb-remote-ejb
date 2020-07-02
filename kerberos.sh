523324edc48b04314b1e8d35e8f21963546e0772
Alessio G. Baroni <alessiogiovanni.baroni@gmail.com>
2020-07-02 15:23:32 +0200

    Added some useful flags to kadmin.local

--- kerberos.sh
+++ kerberos.sh
@@ -14,10 +14,9 @@ cp /vagrant/kdc.conf /var/kerberos/krb5kdc/
 
 kdb5_util -r EXAMPLE.COM create -s -P password
 
-kadmin.local -r EXAMPLE.COM add_principal -pw password admin/admin@EXAMPLE.COM
-kadmin.local -r EXAMPLE.COM add_principal -pw password HTTP/frontend@EXAMPLE.COM
-kadmin.local -r EXAMPLE.COM add_principal -pw password remote/backend@EXAMPLE.COM
-kadmin.local -r EXAMPLE.COM add_principal -pw password user1@EXAMPLE.COM
+kadmin.local -r EXAMPLE.COM add_principal -kvno 0 +allow_forwardable +allow_renewable +allow_proxiable +ok_as_delegate +ok_to_auth_as_delegate -pw password admin/admin@EXAMPLE.COM
+kadmin.local -r EXAMPLE.COM add_principal -kvno 1 +allow_forwardable +allow_renewable +allow_proxiable +ok_as_delegate +ok_to_auth_as_delegate -pw password HTTP/frontend@EXAMPLE.COM
+kadmin.local -r EXAMPLE.COM add_principal -kvno 2 +allow_forwardable +allow_renewable +allow_proxiable +ok_as_delegate +ok_to_auth_as_delegate -pw password remote/backend@EXAMPLE.COM
+kadmin.local -r EXAMPLE.COM add_principal -kvno 3 +allow_forwardable +allow_renewable +allow_proxiable +ok_as_delegate +ok_to_auth_as_delegate -pw password user1@EXAMPLE.COM
 
 systemctl start krb5kdc
-
