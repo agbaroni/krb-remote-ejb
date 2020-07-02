80a41bde3374a83a31b5e211beb167479044fe11
Alessio G. Baroni <alessiogiovanni.baroni@gmail.com>
2020-07-02 09:42:14 +0200

    Applied suggestions by Darran

--- sources/frontend/web/src/main/java/io/github/agbaroni/tests/krb/RemoteTest.java
+++ sources/frontend/web/src/main/java/io/github/agbaroni/tests/krb/RemoteTest.java
@@ -1,5 +1,8 @@
 package io.github.agbaroni.tests.krb;
 
+import java.util.Properties;
+
+import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,7 +12,10 @@ public class RemoteTest {
 
     @GET
     public String getUser() throws Exception {
-	InitialContext ic = new InitialContext();
+        Properties prop = new Properties();
+        prop.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+        prop.put(Context.PROVIDER_URL, "remote+http://backend:8080");
+	InitialContext ic = new InitialContext(prop);
 	Bean b = (Bean) ic.lookup("ejb:krb-propagate-backend-application-0.1.0-SNAPSHOT/io.github.agbaroni.tests-krb-propagate-backend-ejb-0.1.0-SNAPSHOT/BeanImpl!io.github.agbaroni.tests.krb.Bean");
 
 	return b.getUser();
